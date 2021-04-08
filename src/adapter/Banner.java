7210b440ff20cc6d21b96680836f235abe6d34b5
github <kitakazetoshirokuma@gmail.com>
2021-04-08 22:57:30 +0900

    adaoter-pattern2

--- src/adapter/Banner.java
+++ src/adapter/Banner.java
@@ -1,5 +1,15 @@
 package adapter;
 
+//Bannerクラスを使って、Printインターフェースを満たすようなクラスを作る→アダプターを作る（= PrintBannerクラス)
 public class Banner {
-
+	private String string;
+	public Banner(String string) {
+		this.string = string;
+	}
+	public void showWithParen() {
+		System.out.println("(" + string + ")");
+	}
+	public void showWithAster() {
+		System.out.println("*" + string + "*");
+	}
 }
--- src/adapter/PrintBanner.java
+++ src/adapter/PrintBanner.java
@@ -1,5 +1,20 @@
 package adapter;
 
-public class PrintBanner {
+public class PrintBanner extends Banner implements Print{
+
+	//adapterパターン
+	public PrintBanner(String string) {
+		super(string);
+	}
+
+	@Override
+	public void printWeak() {
+		showWithParen();
+	}
+
+	@Override
+	public void printString() {
+		showWithAster();
+	}
 
 }
